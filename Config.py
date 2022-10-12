a48309308d3ca16f41a15078e38c3218b38ae3be
Mohamed Rizad <rizadvillan@gmail.com>
2022-10-12 21:32:17 +0530

    !!!!!

---
+++ Config.py
@@ -0,0 +1,11 @@
+#TELEGRAM CREDENTIALS
+
+API_ID = "<PUT API-ID HERE"
+API_HASH = "<PUT API-HASH HERE>"
+BOT_TOKEN = "<PUT BOT-TOKEN HERE"
+
+
+#CONSTANT VARIABLES
+base_url='https://ifsc.razorpay.com/' 
+head="**Detailed InFo**\n...................\n\n"
+
